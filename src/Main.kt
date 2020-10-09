b65e0d642bd58adcb3da9cb41c41d2ee17baa31b
Ilyosbek Ibrokhimov <46449085+ilyosbekibrohimov@users.noreply.github.com>
2020-10-09 13:02:51 +0900

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,24 @@
+
+val carName = "Damas"
+
+var nonNull: String? = null
+
+
+
+fun main() {
+
+
+
+
+    if(!carName.equals(null)){
+        println(carName)
+    }
+    if(!nonNull.equals(null)){
+        println(nonNull)
+    }
+
+
+
+
+
+}
